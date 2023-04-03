38a12832cecfd6492ff761e736905212967ca456
gcnaveen <naveengc99@gmail.com>
2023-04-03 10:22:00 -0700

    add-all-files

---
+++ models/orderModel.js
@@ -0,0 +1,71 @@
+import mongoose from 'mongoose';
+
+const orderSchema = new mongoose.Schema(
+  {
+    orderItems: [
+      {
+        slug: { type: String, required: true },
+        name: { type: String, required: true },
+        quantity: { type: Number, required: true },
+        image: { type: String, required: true },
+        price: { type: Number },
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'Product',
+          required: true,
+        },
+      },
+    ],
+    shippingAddress: {
+      fullName: { type: String },
+      address: { type: String },
+      city: { type: String },
+      postalCode: { type: String },
+      country: { type: String },
+      location: {
+        lat: Number,
+        lng: Number,
+        address: String,
+        name: String,
+        vicinity: String,
+        googleAddressId: String,
+      },
+    },
+    contactDetails: {
+      phoneNumber: { type: Number, unique: true },
+      email: { type: String, unique: true },
+      address: { type: String, required: true },
+      postalCode: { type: Number, required: true },
+      country: { type: String, required: true },
+      city: { type: String, required: true },
+    },
+    paymentMethod: { type: String },
+    paymentResult: {
+      id: String,
+      status: String,
+      update_time: String,
+      email_address: String,
+    },
+    itemsPrice: { type: Number, required: true },
+    shippingPrice: { type: Number, required: true },
+    // taxPrice: { type: Number, required: true },
+    totalPrice: { type: Number, required: true },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    isPaid: { type: Boolean, default: false },
+    paidAt: { type: Date },
+    isRead: { type: Boolean, default: false },
+    isCancelled: { type: Boolean, default: false },
+    isOrderAccepted: { type: Boolean, default: false },
+    isOrderRejected: { type: Boolean, default: false },
+    isDelivered: { type: Boolean, default: false },
+    isDispatched: { type: Boolean, default: false },
+    isOutForDelivery: { type: Boolean, default: false },
+    deliveredAt: { type: Date },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Order = mongoose.model('Order', orderSchema);
+export default Order;
---
+++ models/productModel.js
@@ -0,0 +1,51 @@
+import mongoose from 'mongoose';
+
+const reviewSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    comment: { type: String, required: true },
+    rating: { type: Number, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const productSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, unique: true },
+    slug: { type: String, required: true, unique: true },
+    image: { type: String },
+    images: [String],
+    brand: { type: String, required: true },
+    category: { type: String, required: true },
+    categoryID: {
+      type: mongoose.Types.ObjectId,
+      required: true,
+      ref: 'Category',
+    },
+    subCategory: { type: String },
+    description: { type: String, required: true },
+    additionalInfo: { type: String},
+    price: { type: Number },
+    packageName:{type:String},
+    priceFor30Pills: { type: Number },
+    priceFor60Pills: { type: Number },
+    priceFor90Pills: { type: Number },
+    priceFor120Pills: { type: Number },
+    priceFor180Pills: { type: Number },
+    priceFor270Pills: { type: Number},
+    countInStock: { type: Number, required: true },
+    productDiscountedPrice: { type: Number, required: true },
+    rating: { type: Number, required: true },
+    numReviews: { type: Number, required: true },
+    reviews: [reviewSchema],
+    blackFridaySale: { type: Boolean , default:false},
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = mongoose.model('Product', productSchema);
+export default Product;
